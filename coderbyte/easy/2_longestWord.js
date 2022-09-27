a5fd294bea3e94af56f514722672cfd9647d1cc9
aghabalaguluzade <agabala.oyunda@gmail.com>
2022-09-27 23:25:13 +0400

    Algorithms-in-JavaScript

---
+++ coderbyte/easy/2_longestWord.js
@@ -0,0 +1,13 @@
+// Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string.
+// If there are two or more words that are the same length, return the first word from the string with that length. 
+// Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"
+
+const longestWord = (sen) => {
+     const arr = sen.replace(/[^a-zA-Z ]/g,"").split(" ");
+     arr.sort((a,b) => {
+          return b.length - a.length;
+     });
+     return arr;
+};
+
+console.log(longestWord("fun&!! time"));
